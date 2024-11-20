7db44dabf58dbbbab33e9a39d96fc6bfd229b7a4
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-11-20 22:41:41 +0530

    navbar

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {assets} from '../assets/assets'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
+  const [user,setUser]=useState(null);
   return (
-    <div>
-      navbar
+    <div className=''>
+      <Link to="/">
+      <img src={assets.logo} alt='' className='w-28 sm:w-32 lg:w-40'/>
+      </Link>
+
+      <div>
+        {user ? 
+        <div></div>
+        :
+        <div>
+          <p>
+            Pricing
+          </p>
+          <button>
+            login
+          </button>
+        </div>
+       }
+        
+        
+      </div>
     </div>
   )
 }

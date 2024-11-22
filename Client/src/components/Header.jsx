b09c714343fd4451680ea8060d0c66f7964b55e0
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-11-22 23:59:09 +0530

    Update Header.jsx

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Header = () => {
   return (
-    <div>
-      header
-    </div>
-  )
-}
+    <div className="flex flex-col justify-center items-center text-center my-20">
+      <div className="text-stone-500 inline-flex text-center gap-2 bg-white py-1 px-6 rounded-full border border-neutral-500">
+        <p>Best text to image generator</p>
+        <img src={assets.star_icon} alt="" />
+      </div>
+      <h1 className="text-4xl max-x-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center">
+        Turn text to <span className="text-blue-600">image</span>, in seconds
+      </h1>
 
-export default Header
+      <p className="text-center max-w-xl mx-auto mt-5">Unleash your creativity with AI. Turn your imagination into visual
+art in seconds - just type, and watch the magic happen.</p>
+
+<button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full">Generate Images
+    <img src={assets.star_group} alt="" />
+</button>
+    </div>
+  );
+};
+
+export default Header;

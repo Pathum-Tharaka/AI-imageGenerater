bc66c3549ecfbf4830fd02478ecfd4f87a7205bb
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-11-24 15:54:46 +0530

    Update Testimonials.jsx

--- Client/src/components/Testimonials.jsx
+++ Client/src/components/Testimonials.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { assets,testimonialsData } from '../assets/assets'
+import { motion } from 'framer-motion'
 
 const Testimonials = () => {
   return (
-    <div className="flex flex-col justify-center items-center my-20 py-12">
+    <motion.div 
+    initial={{ opacity: 0.2, y: 100 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{ duration: 1 }}
+    viewport={{ once: true }}
+    className="flex flex-col justify-center items-center my-20 py-12">
       <h1 className="text-3xl sm:text-4xl mb-2 font-semibold">
       Customer testimonials      </h1>
       <p className="mb-12 text-gray-500">What Our Users Are Saying</p>
@@ -28,7 +34,7 @@ const Testimonials = () => {
             ))}
         </div>
 
-    </div>
+    </motion.div>
   )
 }
 

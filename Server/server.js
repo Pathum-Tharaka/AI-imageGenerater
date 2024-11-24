55bd0fd9d41bf94f3bc99372c2de55da2bd0337c
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-11-24 23:23:10 +0530

    Update server.js

---
+++ Server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import 'dotenv/config';
+import cors from "cors";
+
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
+
+
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+await connectDB();
+
+app.use('/api/user', userRouter);
+app.get('/', (req, res) => res.send('API is running'));
+
+
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT));

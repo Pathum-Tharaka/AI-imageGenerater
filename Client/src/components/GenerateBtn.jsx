2f08976960ed30411fd04f50e2f8a8629e64c3c6
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-11-24 15:55:03 +0530

    Update GenerateBtn.jsx

--- Client/src/components/GenerateBtn.jsx
+++ Client/src/components/GenerateBtn.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const GenerateBtn = () => {
+  const {user,setShowLogin} = useContext(AppContext);
+  const Navigate = useNavigate();
+
+  const onClickHandler = () => {
+    if(user) {
+      Navigate("/result")
+    } else {
+      setShowLogin(true)
+    }
+  }
+
   return (
-    <div className='pb-16 text-center'>
+    <motion.div 
+    initial={{ opacity: 0.2, y: 100 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{ duration: 1 }}
+    viewport={{ once: true }}
+    className='pb-16 text-center'>
       <h1 className='text-2xl md:text-3xl lg:text-4xl mt-4 text-neutral-800 py-6 md:py-16 font-semibold'>
         See the magic. Try now
       </h1>
-      <button className='inline-flex items-center gap-2 bg-black text-white px-12 py-3 m-auto rounded-full hover:scale-105 transition-all duration-500'>
+      <button onClick={onClickHandler} className='inline-flex items-center gap-2 bg-black text-white px-12 py-3 m-auto rounded-full hover:scale-105 transition-all duration-500'>
         Generate Images
         <img src={assets.star_group} alt="" className='h-6'/>
       </button>
-    </div>
+    </motion.div>
   )
 }
 

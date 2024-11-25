a4e683fe2308bc79c7c918c4dec267be5d3ad000
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-11-26 00:06:41 +0530

    Update Login.jsx

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,56 +1,57 @@
 import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
-import {motion} from 'framer-motion'
-import axios from 'axios'
+import { motion } from "framer-motion";
+import axios from "axios";
 import { toast } from "react-toastify";
 
-
 const Login = () => {
-
-    
   const [state, setState] = useState("Login");
-  const {setShowLogin,backendUrl,setToken,setUser} = useContext(AppContext)
+  const { setShowLogin, backendUrl, setToken, setUser } =
+    useContext(AppContext);
 
-  const[name,setName] = useState("")
-  const[email,setEmail] = useState("")
-  const[password,setPassword] = useState("")
-
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
 
     try {
       if (state === "Login") {
-        const {data} = await axios.post(backendUrl + "/api/user/login", {email, password});
+        const { data } = await axios.post(backendUrl + "/api/user/login", {
+          email,
+          password,
+        });
 
-        if(data.success){
-          setToken(data.token)
-          setUser(data.user)
-          localStorage.setItem("token",data.token)
+        if (data.success) {
+          setToken(data.token);
+          setUser(data.user);
+          localStorage.setItem("token", data.token);
           setShowLogin(false);
-        }else{
-          toast.error(data.message)
+        } else {
+          toast.error(data.message);
         }
-  }else{
-    const {data} = await axios.post(backendUrl + "/api/user/register", {name, email, password});
+      } else {
+        const { data } = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
 
-    if(data.success){
-      setToken(data.token)
-      setUser(data.user)
-      localStorage.setItem("token",data.token)
-      setShowLogin(false);
-    }else{
-      toast.error(data.message)
-    }
-  }
-
+        if (data.success) {
+          setToken(data.token);
+          setUser(data.user);
+          localStorage.setItem("token", data.token);
+          setShowLogin(false);
+        } else {
+          toast.error(data.message);
+        }
+      }
     } catch (error) {
-      toast.error(error.message)
+      toast.error(error.message);
     }
   };
-
-
 
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -59,16 +60,16 @@ const Login = () => {
     };
   }, []);
 
-
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex items-center justify-center">
-      <motion.form 
-      onSubmit={onSubmitHandler}
-      initial={{ opacity: 0.2, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.3 }}
-      viewport={{ once: true }}
-      className="relative bg-white p-10 rounded-xl text-slate-500">
+      <motion.form
+        onSubmit={onSubmitHandler}
+        initial={{ opacity: 0.2, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3 }}
+        viewport={{ once: true }}
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
         <h1 className="text-center text-2xl text-neutral-700 font-medium">
           {state}
         </h1>
@@ -122,16 +123,26 @@ const Login = () => {
         {state === "Login" ? (
           <p className="text-center mt-5">
             Don't have an account?{" "}
-            <span className="text-blue-600 cursor-pointer" onClick={() => setState("Sign up")}>Sign up</span>
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Sign up")}
+            >
+              Sign up
+            </span>
           </p>
         ) : (
           <p className="text-center mt-5">
             Already have an account?{" "}
-            <span className="text-blue-600 cursor-pointer" onClick={() => setState("Login")}>Login</span>
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Login")}
+            >
+              Login
+            </span>
           </p>
         )}
         <img
-        onClick={() => setShowLogin(false)}
+          onClick={() => setShowLogin(false)}
           className="absolute top-5 right-5 cursor-pointer"
           src={assets.cross_icon}
           alt=""
@@ -139,6 +150,6 @@ const Login = () => {
       </motion.form>
     </div>
   );
-} 
+};
 
 export default Login;

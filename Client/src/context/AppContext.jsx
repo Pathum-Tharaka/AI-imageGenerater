34292299409b01e15fa6d767aa7838a8e267ec1b
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-11-25 15:21:34 +0530

    Update AppContext.jsx

--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export const AppContext = createContext()
 
@@ -6,10 +8,51 @@ const AppContextProvider = (props)=>{
     
     const [user,setUser]= useState(null)
     const [showLogin,setShowLogin] = useState(false)
+    const [token,setToken] = useState(localStorage.getItem('token'))
+
+    const [credit,setCredit] = useState(false)
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+
+    const loadCreditData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + "/api/user/credits", { headers: {token} });
+            if(data.success){
+                setCredit(data.credits)
+                setUser(data.user)
+            }
+        } catch (error) {
+            console.error(error);
+            toast.error(error.message);
+        }
+    };
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        setToken("");
+        setUser(null);
+        setShowLogin(false);
+    }
+
+    useEffect(() => {
+        if (token) {
+            loadCreditData();
+        }
+
+    }, [token]);
+
+
+
 
     const value ={
         user,setUser,
-        showLogin,setShowLogin
+        showLogin,setShowLogin,
+        backendUrl,
+        token,setToken,
+        credit,setCredit,
+        loadCreditData,
+        logout
     }
 
     return(

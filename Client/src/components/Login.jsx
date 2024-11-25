02221e51f94e7c376f04b655ffff13fa3ffa4d27
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-11-25 15:21:26 +0530

    Update Login.jsx

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -2,13 +2,55 @@ import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
 import {motion} from 'framer-motion'
+import axios from 'axios'
+import { toast } from "react-toastify";
 
 
 const Login = () => {
 
     
   const [state, setState] = useState("Login");
-  const {setShowLogin} = useContext(AppContext)
+  const {setShowLogin,backendUrl,setToken,setUser} = useContext(AppContext)
+
+  const[name,setName] = useState("")
+  const[email,setEmail] = useState("")
+  const[password,setPassword] = useState("")
+
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (state === "Login") {
+        const {data} = await axios.post(backendUrl + "/api/user/login", {email, password});
+
+        if(data.success){
+          setToken(data.token)
+          setUser(data.user)
+          localStorage.setItem("token",data.token)
+          setShowLogin(false);
+        }else{
+          toast.error(data.message)
+        }
+  }else{
+    const {data} = await axios.post(backendUrl + "/api/user/register", {name, email, password});
+
+    if(data.success){
+      setToken(data.token)
+      setUser(data.user)
+      localStorage.setItem("token",data.token)
+      setShowLogin(false);
+    }else{
+      toast.error(data.message)
+    }
+  }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  };
+
+
 
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -21,6 +63,7 @@ const Login = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex items-center justify-center">
       <motion.form 
+      onSubmit={onSubmitHandler}
       initial={{ opacity: 0.2, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.3 }}
@@ -35,6 +78,8 @@ const Login = () => {
           <div className="border px-4 py-2 flex items-center gap-2 mt-5 rounded-full">
             <img className="w-5" src={assets.profile_icon} alt="" />
             <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
               className="outline-none text-sm"
               type="text"
               placeholder="Full Name"
@@ -46,6 +91,8 @@ const Login = () => {
         <div className="border px-4 py-2 flex items-center gap-2 mt-4 rounded-full">
           <img src={assets.email_icon} alt="" />
           <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             className="outline-none text-sm"
             type="email"
             placeholder="Email id"
@@ -56,6 +103,8 @@ const Login = () => {
         <div className="border px-4 py-2 flex items-center gap-2 mt-4 rounded-full">
           <img src={assets.lock_icon} alt="" />
           <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
             className="outline-none text-sm"
             type="password"
             placeholder="Password"
@@ -90,6 +139,6 @@ const Login = () => {
       </motion.form>
     </div>
   );
-};
+} 
 
 export default Login;

3053e575d97022aaa47ae029ae97a24fbc142eff
pathumtharaka2000 <it22600516@my.sliit.lk>
2024-11-25 15:21:52 +0530

    Update auth.js

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -1,13 +1,28 @@
+import e from "express";
 import jwt from "jsonwebtoken";
 
 
 const userAuth = async (req, res, next) => {
+
+        const{token} = req.headers;
+
+
+        if(!token){
+            return res.status(401).json({ success: false, message: "Unauthorized login again" });
+        }
+
     try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
         
-        const token = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decodedToken;
+        if(tokenDecode.id){
+            req.body.userId = tokenDecode.id;
+        }
+        else{
+            return res.status(401).json({ success: false, message: "Unauthorized login again" });
+        }
+
         next();
+        
     } catch (error) {
         console.log(error);
         res.status(401).json({ success: false, message: "Unauthorized" });
